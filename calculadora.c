a1631b18db063f707c84f05852a3c281ebce3de4
biro24 <94942087+biro24@users.noreply.github.com>
2022-09-07 13:31:26 -0500

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,62 +1,68 @@
 #include <stdio.h>
-#include <math.h>
+#include <conio.h>
 
+int main()
+{
+    char opcion;
+    int n1, n2;
 
-void main(void){
-int n1,n2,i,j,a,b,suma,mul,res,div;
-
-  do{
-    do{
-      printf("BIENVENIDO A LA CALCULADORA");
-      printf("Introduce un número Para seleccionar una opción del menú:");
-      printf("1.-Suma");
-      printf("2.-Multiplicación");
-      printf("3.-Resta");
-      printf("4.-División");
-      scanf("%d",n1);
-      }
-    while(n1<=4);
-  
-    Switch(n1)
+    do
     {
-    Case1:
-      printf("Introduce el 1er número a sumar:");
-      scanf("%d",a);
-      printf("/nIntroduce el 2do número a sumar:");
-      scanf("%d",b);
-      suma=a+b;
-      printf("Tu resultado es:%d",suma);
-      break;
-    Case2:
-      printf("Introduce el Multiplicando:");
-      scanf("%d",a);
-      printf("/nIntroduce el Multiplicador:");
-      scanf("%d",b);
-      mul=a*b;
-      printf("Tu resultado es:%d",mul);
-      break;
-    Case3:
-      printf("Introduce el Número a Restar:");
-      scanf("%d",a);
-      printf("/nIntroduce el Restador:");
-      scanf("%d",b);
-      res=a-b;
-      printf("Tu resultado es:%d",res);
-      break;
-    Case4:
-      printf("Introduce el Dividendo:");
-      scanf("%d",a);
-      printf("/nIntroduce el Divisor:");
-      scanf("%d",b);
-      div=a/b;
-      break;
-      printf("Tu resultado es:%d",div);
-    Default:
-      printf("No está en la lista, deseas hacer otra operación?");
-      printf("Introduce 1=Si, Otro Número=No");
-      scanf("%d",n2);
-    }
-  }
-  while(n2==1);
+        printf( "\n   >>> MENU CALCULADORA <<<" );
+        printf( "\n\n   1. Sumar dos n%cmeros.", 163 );
+        printf( "\n   2. Restar dos n%cmeros.", 163 );
+        printf( "\n   3. Multiplicar dos n%cmeros.", 163 );
+        printf( "\n   4. Dividir dos n%cmeros.", 163 );
+        printf( "\n   5. Salir.\n" );
 
+        /* Filtramos la opción elegida por el usuario */
+        do
+        {
+            printf( "\n   Introduzca opci%cn (1-5): ", 162 );
+            fflush( stdin );
+            scanf( "%c", &opcion);
+
+        } while ( opcion < '1' || opcion > '5' );
+        /* La opción sólo puede ser '1', '2', '3', '4' o '5' */
+
+        switch ( opcion )
+        {
+                      /* Opción 1: Sumar */
+            case '1': printf( "\n   Introduzca primer sumando: " );
+                      scanf( "%d", &n1);
+                      printf( "\n   Introduzca segundo sumando: " );
+                      scanf( "%d", &n2);
+                      printf( "\n   %d + %d = %d\n", n1, n2, n1 + n2 );
+                      break;
+
+                      /* Opción 2: Restar */
+            case '2': printf( "\n   Introduzca minuendo: " );
+                      scanf( "%d", &n1);
+                      printf( "\n   Introduzca sustraendo: " );
+                      scanf( "%d", &n2);
+                      printf( "\n   %d - %d = %d\n", n1, n2, n1 - n2 );
+                      break;
+
+                      /* Opción 3: Multiplicar */
+            case '3': printf( "\n   Introduzca primer operando: " );
+                      scanf( "%d", &n1);
+                      printf( "\n   Introduzca segundo operando: " );
+                      scanf( "%d", &n2);
+                      printf( "\n   %d * %d = %d\n", n1, n2, n1 * n2 );
+                      break;
+
+                      /* Opción 4: División entera */
+            case '4': printf( "\n   Introduzca dividendo: " );
+                      scanf( "%d", &n1);
+                      printf( "\n   Introduzca divisor: " );
+                      scanf( "%d", &n2);
+                      if ( n2 != 0 )
+                          printf( "\n   %d div %d = %d ( Resto = %d )\n", n1, n2, n1 / n2, n1 % n2 );
+                      else
+                          printf( "\n   ERROR: No se puede dividir entre cero.\n" );
+        }
+
+    } while ( opcion != '5' );
+
+    return 0;
 }
